f61a92ed02ca27d6f09585735870fd39db8d4cf9
Kshitij Agarwal <kshitija024@gmail.com>
2024-01-09 14:24:28 +0530

    Added Files

---
+++ src/utils/fetchData.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "a30838ff00mshbc23407d139ec1bp132d16jsnb01e358a6a21",
+  },
+};
+
+
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
